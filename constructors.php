74e480a4ae39c36b505f76aa83b5e4ce4cb1fa60
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-01-10 10:30:45 -0600

    Update constructors.php

--- constructors.php
+++ constructors.php
@@ -1,1 +1,15 @@
-
+class SubjectNav {
+	function __construct($subject){
+        $servername = "localhost";
+        $username = "calcs";
+        $password = "Muaythai97!@";
+        $database = "calcs";
+        $conn = new mysqli($servername, $username, $password, $database);
+        $this->subject = $subject;
+        $sql = "SELECT * FROM `Navbar_Contents` WHERE `subject` = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $this->subject);
+        $stmt->execute();
+        $this->result = $stmt->get_result();
+        $this->conn = $conn;
+}
